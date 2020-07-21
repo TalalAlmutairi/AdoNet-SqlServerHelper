126043bf97f7349def1089e06195bd2e20161a33
TalalAlmutairi <Almutiri.Talal@hotmail.com>
2020-07-21 16:48:14 +0300

    Add project files.

---
+++ AdoSqlServerHelper/Examples.aspx.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace AdoSqlServerHelper
+{
+    public partial class Examples : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Getting all employees data
+        protected void btnLoadData_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM Employees";
+            GridView1.DataSource = SqlHelper.ExecuteQuery(sql, CommandType.Text, null);
+            GridView1.DataBind();
+        }
+
+        //Selecting only the employee with id=1
+        protected void btnSqlWhere_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM Employees WHERE EmpID=@ID";
+
+            // SqlParameter uses for security to prevent SQL injection
+            //@ID in the SQL above must match the parameter in SqlParameter
+            SqlParameter[] parametersList = new SqlParameter[]{
+                new SqlParameter ("@ID","1"), // you can read it from input or Textbox
+           };
+
+            GridView1.DataSource = SqlHelper.ExecuteQuery(sql, CommandType.Text, parametersList);
+            GridView1.DataBind();
+        }
+
+        //Getting the maximum salary of all employees as only one value
+        protected void btnExecuteScalar_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT MAX(Age) FROM Employees";
+            lbMsg.Text = SqlHelper.ExecuteScalar(sql, CommandType.Text, null);
+            
+        }
+
+        //Execute stored procedure
+        protected void btnSP_Click(object sender, EventArgs e)
+        {
+            GridView1.DataSource = SqlHelper.ExecuteQuery("GetAllEmployees", CommandType.StoredProcedure, null);
+            GridView1.DataBind();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            string sql = "INSERT INTO Employees VALUES(@FName,@LName,@Age,@CountryID)";
+            SqlParameter[] parametersList = new SqlParameter[]{
+                new SqlParameter ("@FName",txtFName.Value),
+                new SqlParameter ("@LName",txtLName.Value),
+                new SqlParameter ("@Age",txtAge.Value),
+                new SqlParameter ("@CountryID",ddlCouontries.Value),
+           };
+
+            if (SqlHelper.ExecuteNonQuery(sql, CommandType.Text, parametersList))
+                lbMsg.Text = "Inserted successfully";
+            else
+                lbMsg.Text = "Error";
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string sql = "UPDATE Employees SET FirstName=@FName,LastName=@LName,Age=@Age,CountryID=@CountryID WHERE EmpID =@ID";
+            SqlParameter[] parametersList = new SqlParameter[]{
+                new SqlParameter ("@ID",txtEmpID.Value),
+                new SqlParameter ("@FName",txtFName.Value),
+                new SqlParameter ("@LName",txtLName.Value),
+                new SqlParameter ("@Age",txtAge.Value),
+                new SqlParameter ("@CountryID",ddlCouontries.Value),
+           };
+
+            if (SqlHelper.ExecuteNonQuery(sql, CommandType.Text, parametersList))
+                lbMsg.Text = "Updated successfully";
+            else
+                lbMsg.Text = "Error";
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            string sql = @"DELETE FROM Employees WHERE EmpID =@ID";
+
+            SqlParameter[] parametersList = new SqlParameter[]{
+                    new SqlParameter ("@ID",txtEmpID.Value),
+                };
+
+            if (SqlHelper.ExecuteNonQuery(sql, CommandType.Text, parametersList))
+                lbMsg.Text = "Deleted successfully";
+            else
+                lbMsg.Text = "Error";
+        }
+
+        //'Execute two SQL statements Insert and update, which all SQL statements in a single transaction, rolling back if an error has occurred
+        protected void btnExecuteTransaction_Click(object sender, EventArgs e)
+        {
+            ArrayList listOfSQLs = new ArrayList();
+            List<SqlParameter[]> listOfParamerters = new List<SqlParameter[]>();
+
+            string sql1 = "INSERT INTO Employees VALUES(@FName,@LName,@Age,@CountryID)";
+            SqlParameter[] parameters1 = new SqlParameter[]{
+                new SqlParameter ("@FName","Test F Name"),
+                new SqlParameter ("@LName","Test L Name"),
+                new SqlParameter ("@Age",25),
+                new SqlParameter ("@CountryID",1),
+           };
+
+            listOfSQLs.Add(sql1);
+            listOfParamerters.Add(parameters1);
+
+
+            string sql2 = "UPDATE Employees SET FirstName=@FName,LastName=@LName,Age=@Age,CountryID=@CountryID WHERE EmpID =@ID";
+            SqlParameter[] parameters2 = new SqlParameter[]{
+                new SqlParameter ("@ID",4),
+                new SqlParameter ("@FName","New F Name"),
+                new SqlParameter ("@LName","New L Name"),
+                new SqlParameter ("@Age",30),
+                new SqlParameter ("@CountryID",2),
+           };
+
+
+            listOfSQLs.Add(sql2);
+            listOfParamerters.Add(parameters2);
+
+            if (SqlHelper.ExecuteTransaction(listOfSQLs, listOfParamerters))
+                lbMsg.Text = "All SQL statements executed successfully";
+            else
+                lbMsg.Text = "Error";
+           
+        }
+
+        //Execute two select queries, and returns employees and country tables
+        protected void btnReturnDS_Click(object sender, EventArgs e)
+        {
+            // You can also use a stored procedure instead  of two queries in one string variable
+            string sql = "SELECT * FROM Employees; SELECT * FROM Country";
+            DataSet ds = SqlHelper.ExecuteQueryDS(sql, CommandType.Text, null);
+            GridViewEmp.DataSource = ds.Tables[0];
+            GridViewEmp.DataBind();
+
+            GridViewCountry.DataSource = ds.Tables[1];
+            GridViewCountry.DataBind();
+        }
+    }
+}
---
+++ AdoSqlServerHelper/SqlHelper.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AdoSqlServerHelper
+{
+    public class SqlHelper
+    {
+        // You need to change connection string from Web.config
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["DbConnecion"].ConnectionString;
+        /// <summary>
+        /// Execute Select query and return results as a DataTable
+        /// </summary>
+        /// <param name="cmdText"></param>
+        /// <param name="cmdType"></param>
+        /// <param name="parameters"></param>
+        /// <returns>DataTable</returns>
+        public static DataTable ExecuteQuery(string cmdText, CommandType cmdType, SqlParameter[] parameters)
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(cmdText, con))
+                    {
+                        con.Open();
+                        cmd.CommandType = cmdType;
+
+                        if (parameters != null)
+                            cmd.Parameters.AddRange(parameters);
+
+                        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                        adapter.Fill(table);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Save ex to logger
+                string error = ex.Message;
+                
+                return null;
+            }
+            return table;
+        }
+        /// <summary>
+        ///  Executes a SQL statement and returns the number of rows affected. NonQuery (Insert, update, and delete)
+        /// </summary>
+        /// <param name="cmdText"></param>
+        /// <param name="cmdType"></param>
+        /// <param name="parameters"></param>
+        /// <returns>bool</returns>
+        public static bool ExecuteNonQuery(string cmdText, CommandType cmdType, SqlParameter[] parameters)
+        {
+            var value = 0;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(cmdText, con))
+                    {
+                         con.Open();
+                        cmd.CommandType = cmdType;
+                        if (parameters != null)
+                            cmd.Parameters.AddRange(parameters);
+                        value =  cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Save ex to logger
+                string error = ex.Message;
+                return false;
+            }
+            if (value < 0)
+                return false;
+            else
+                return true;
+        }
+        /// <summary>
+        /// Executes the query, and returns the first column of the first row in the result set returned by the query.
+        /// </summary>
+        /// <param name="cmdText"></param>
+        /// <param name="cmdType"></param>
+        /// <param name="parameters"></param>
+        /// <returns>string</returns>
+        public static string ExecuteScalar(string cmdText, CommandType cmdType, SqlParameter[] parameters)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(cmdText, con))
+                    {
+                        con.Open();
+                        cmd.CommandType = cmdType;
+                        if (parameters != null)
+                            cmd.Parameters.AddRange(parameters);
+                        string value = cmd.ExecuteScalar().ToString();
+
+                        return value;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Save ex to logger
+                string error = ex.Message;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Executes the query, and returns multiple tables
+        /// </summary>
+        /// <param name="cmdText"></param>
+        /// <param name="cmdType"></param>
+        /// <param name="parameters"></param>
+        /// <returns>DataSet</returns>
+        public static DataSet ExecuteQueryDS(string cmdText, CommandType cmdType, SqlParameter[] parameters)
+        {
+            DataSet tables = new DataSet();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(cmdText, con))
+                    {
+                        con.Open();
+                        cmd.CommandType = cmdType;
+
+                        if (parameters != null)
+                            cmd.Parameters.AddRange(parameters);
+
+                        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                        adapter.Fill(tables);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Save ex to logger
+                string error = ex.Message;
+                return null;
+            }
+            return tables;
+        }
+        /// <summary>
+        /// Execute multiple SQL statements such as Insert, update, and delete, which all SQL statements in a single transaction, rolling back if an error has occurred
+        /// </summary>
+        /// <param name="cmdText"></param>
+        /// <param name="paramerters"></param>
+        /// <returns>bool</returns>
+        public static bool ExecuteTransaction(ArrayList cmdText, List<SqlParameter[]> paramerters)
+        {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                SqlTransaction transaction = null;
+                cmd.Connection = con;
+
+                try
+                {
+                    con.Open();
+
+                    transaction = con.BeginTransaction(IsolationLevel.ReadCommitted);
+
+                    // Assign transaction object for a pending local transaction.
+                    cmd.Connection = con;
+                    cmd.Transaction = transaction;
+                    // cmd.BindByName = true;
+                    for (int i = 0; i < cmdText.Count; i++)
+                    {
+                        cmd.CommandText = cmdText[i].ToString();
+                        if (paramerters[i].Length > 0)
+                            cmd.Parameters.AddRange(paramerters[i]);
+
+                        int flag = cmd.ExecuteNonQuery();
+                        cmd.Parameters.Clear();
+                        if (flag < 1)
+                            return false;
+                    }
+
+                    transaction.Commit();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    // Save ex to logger
+                    string error = ex.Message;
+                    return false;
+                }
+            }
+        }
+    }
+}
